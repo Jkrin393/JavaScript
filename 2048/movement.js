3fb808554c445dd5e3fb3584f140b871a4f1c917
Josh Krinsky <95884174+Jkrin393@users.noreply.github.com>
2024-03-26 18:05:55 -0400

    added mergeRow and filterZero, logic seems to work now!

--- 2048/movement.js
+++ 2048/movement.js
@@ -1,6 +1,19 @@
 //new idea, all tile moves are a left move, rotate grid to allow this. Don't need to worry about out of bounds conditions
 
 
+function filterZeros(row){
+    
+    
+    let filteredRow = row.filter(val => val !==0);
+    let emptyValueCount = BOARD_SIZE - filteredRow.length;
+
+    while(BOARD_SIZE - emptyValueCount !== BOARD_SIZE){
+        filteredRow.push(0);
+        emptyValueCount = BOARD_SIZE - filteredRow.length;
+    }
+
+    return filteredRow;
+}
 
 function rotateBoard(gameboard){
     
@@ -20,14 +33,16 @@ function rotateBoard(gameboard){
 //check for mergable values starting at col 1. If it can merge, double in place, push 0, col[row-1] is pushes off the array
 function mergeRow(row){
     
-    for(let col = 1;col < BOARD_SIZE;col++){
-        row[col] += row[col];
-        row.push(0);
+    for(let col = 0;col < BOARD_SIZE;col++){
+        if(row[col] === row[col+1] && row[col] !==0){//need to consider endless zero + zero case
+            row[col] *= 2;
+            row[col+1] = 0;
+        }
     }
-
     return row;
 
 }
+
 
 
 function moveAndMerge(direction, gameBoard){
@@ -46,26 +61,25 @@ function moveAndMerge(direction, gameBoard){
         gameBoard = rotateBoard(gameBoard);
 
     }
+    //this loop simulates the sliding movement across multilple cells. logic: filter out the zero values and create a new arr, push(0) each time a zero value is found
+    for (let i = 0;i<BOARD_SIZE; i++) {
+        let row = gameBoard[i];
+        //row = row.filter(val => val !==0);
+        row  = filterZeros(row);
+        row = mergeRow(row);
+        row = filterZeros(row);
 
-    for (let row of gameBoard) {
-        for (let col = 1; col < BOARD_SIZE; col++) {
-            
-            if(i=0){
-                //logik
-            }
-
-
-            if(col + 1 >= BOARD_SIZE){
-                row[col] = 0;
-                //want to do this with push(0) instead
-            }
-            else if(row[col] === row[col+1]){
-                row[col+1]*=2;
-                row.push(0);
-                
-            }
-        }
+        gameBoard[i] = row;
     }
+
+    //add any valid matching tiles in the row 
+    for(let i = 0; i<BOARD_SIZE;i++){
+        mergeRow(gameBoard[i]);
+
+    }
+
+
+
     //rotate to original position
     for(let i = 0; i<BOARD_SIZE - numRotations;i++){
         gameBoard = rotateBoard(gameBoard);
